d14f409920114a8f6edda9ca3995b7c37379b840
YiLun H <monkey01779@gmail.com>
2022-01-12 20:30:45 +0800

    C#practice
    等額本息攤還試算，每月攤還相同金額，輸入貸款金額、期限以及年利率，得到月付款/總付款/全部應付利息結果。

--- C#practice-Loan.cs
+++ C#practice-Loan.cs
@@ -20,12 +20,12 @@ namespace Homework02_Loan
         private void butmonth_Click(object sender, EventArgs e)
         {
             this.txtmoney.Clear();
-            double loan = double.Parse(txtloan.Text);
-            int month = int.Parse(txtmonth.Text);
-            double rate = 1.0 + double.Parse(txtrate.Text) / 100 / 12;
+            double loan = double.Parse(txtloan.Text);//輸入貸款金額
+            int month = int.Parse(txtmonth.Text);//輸入應繳月份
+            double rate = 1.0 + double.Parse(txtrate.Text) / 100 / 12;//輸入年利率並轉換成月利率
             double rpn = Math.Pow(rate, month);
-            double pmt = loan * rpn * (rate - 1) / (rpn - 1);
-            double totalpayment = pmt * month;
+            double pmt = loan * rpn * (rate - 1) / (rpn - 1);//每月應還額度公式計算
+            double totalpayment = pmt * month;//總應付額度
             double interest = (totalpayment - loan);
             double output = Math.Round(interest, 1);
             this.txtmoney.Text += output;
@@ -72,3 +72,4 @@ namespace Homework02_Loan
         }
     }
 }
+//更新版將公式寫成方法
